b7c37c556170719721bab5905604c7577557fd71
clucas <clucas@grouphh.com>
2024-04-12 21:59:09 +0100

    Complete exercises final

--- LUCAS_CARLOS_test_javascript_sudoku_ex3.js
+++ LUCAS_CARLOS_test_javascript_sudoku_ex3.js
@@ -7,7 +7,7 @@ array_number[4] = "7 4 1 6 3 9 2 5 8";
 array_number[5] = "5 6 8 2 4 7 1 3 9";
 array_number[6] = "8 7 2 9 1 3 6 4 5";
 array_number[7] = "3 9 5 4 2 6 8 7 1";
-array_number[8] = "6 1 4 5 7 8 3 9 2";
+array_number[8] = "6 1 4 5 7 8 3 9 6";
 
 function to_check_table_type(table) {
 
